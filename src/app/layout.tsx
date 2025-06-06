64538db4dc2cc061458eec1d5ebd617444162c6c
vishwa.vj <iamvishwajith.s@gmail.com>
2025-06-06 23:01:28 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
-
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
