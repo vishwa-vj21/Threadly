f19f74c318348709ac3dffb77b1a2788ac55685f
vishwa.vj <iamvishwajith.s@gmail.com>
2025-06-11 03:39:43 +0530

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,30 @@
+
 import Link from "next/link";
 import { Icons } from "@/components/Icons";
 import { buttonVariants } from "./ui/Button";
-const Navbar = () => {
+import { getAuthSession } from "@/lib/auth";
+import { UserAccountNav } from "./UserAccountNav";
+const Navbar = async () => {
+  const session = await getAuthSession();
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
         {/* logo */}
-        <Link href="/" className="flex items-center">
+        <Link href="/" className="flex items-center gap-2">
           <Icons.logo className="h-8 w-8 sm:h-6 sm:2-6"> </Icons.logo>
-          <p className="hidden text-zinc-700 text-sm md:block">Breadit</p>
+          <p className="hidden text-zinc-700 text-sm font-medium md:block">
+            Breadit
+          </p>
         </Link>
 
         {/* search bar */}
-
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign in
-        </Link>
+        {session?.user ? (
+          <UserAccountNav user={session.user} />
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign in
+          </Link>
+        )}
       </div>
     </div>
   );
