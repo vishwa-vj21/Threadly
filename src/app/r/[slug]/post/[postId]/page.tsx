22a3e036e439b6f9cda51a33209bdb596ecea43f
vishwa.vj <iamvishwajith.s@gmail.com>
2025-07-15 20:04:05 +0530

    detailed view

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,8 +1,14 @@
+import PostVoteClient from "@/components/post-vote/PostVoteClient";
+import PostVoteServer from "@/components/post-vote/PostVoteServer";
+import { buttonVariants } from "@/components/ui/Button";
 import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
 import { CachedPost } from "@/types/redis";
 import { Post, User, Vote } from "@prisma/client";
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
+import { classNames } from "uploadthing/client";
 
 interface PageProps {
   params: {
@@ -33,9 +39,47 @@ const page = async ({ params }: PageProps) => {
 
     return (
       <div>
-        <div></div>
+        <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+          <Suspense fallback={<PostVoteShell />}>
+            {/* @ts-expect-error server component */}
+            <PostVoteServer
+              postId={post?.id ?? cachedPost.id}
+              getData={async () => {
+                return await db.post.findUnique({
+                  where: {
+                    id: params.postId,
+                  },
+                  include: {
+                    votes: true,
+                  },
+                });
+              }}
+            />
+          </Suspense>
+        </div>
       </div>
     );
   }
 };
+
+function PostVoteShell() {
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      {/* upvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+
+      {/* score */}
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+
+      {/* downvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+}
 export default page;
