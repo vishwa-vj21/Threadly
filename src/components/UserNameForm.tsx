2aef72638fcba97de0853710407004c0d7cbfc3c
vishwa.vj <iamvishwajith.s@gmail.com>
2025-07-22 05:20:08 +0530

    search bar added

--- src/components/UserNameForm.tsx
+++ src/components/UserNameForm.tsx
@@ -1,9 +1,22 @@
+import { UsernameRequest, UsernameValidator } from "@/lib/validators/username";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { User } from "@prisma/client";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
 
-interface UserNameFormProps {}
+interface UserNameFormProps {
+  user: Pick<User, "id" | "username">;
+}
 
-const UserNameForm: FC<UserNameFormProps> = ({}) => {
-  return <div>UserNameForm</div>;
+const UserNameForm: FC<UserNameFormProps> = ({ user }) => {
+  const {} = useForm<UsernameRequest>({
+    resolver: zodResolver(UsernameValidator),
+    defaultValues: {
+      name: user.username || "", // Default to user's current username
+    },
+  });
+
+  return <form></form>;
 };
 
 export default UserNameForm;
